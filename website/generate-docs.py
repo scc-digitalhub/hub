59c0f35a8c4ae7f7d59cdd6358ed5a5f0de599cb
alb-car <alb.carotti@gmail.com>
2026-03-06 10:33:01 +0100

    another change for action

--- website/generate-docs.py
+++ website/generate-docs.py
@@ -151,6 +151,7 @@ def main():
                     structure_entry = definition
                     if 'metadata' in structure_entry:
                         structure_entry['metadata']['path'] = f'{c}/{t}'
+                        catalog_url = structure['catalog_url']
                         structure_entry['metadata']['repository'] = f'{structure['catalog_url']}/{c}/{t}'
                         structure_entry['metadata']['relationships'] = [{'type': 'part_of', 'dest': hub_ref(c, t, structure_entry['metadata'])}]
                     structure_catalog[c].append(structure_entry)
